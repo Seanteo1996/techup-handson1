29719c7ce6ffed2cb9bfbad4bb2fff0c6b54e0f9
Seanteo1996 <seanteo1996@gmail.com>
2024-11-11 06:21:10 +0000

    Pending Agency Filter look and feel

--- Mentoring Match/mentor script.js
+++ Mentoring Match/mentor script.js
@@ -248,6 +248,23 @@ function updatePaginationControls() {
     }
 }
 
+// JavaScript for Collapsible Filters
+document.addEventListener("DOMContentLoaded", function () {
+    var coll = document.getElementsByClassName("collapsible");
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+});
+
+
 // Function to check if the profile's years of experience matches the selected range
 function checkExperience(profileExperience) {
     console.log('Profile Experience:', profileExperience);
